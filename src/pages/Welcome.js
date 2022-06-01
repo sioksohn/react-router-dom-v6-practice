dad025b5cf1e57c99e71ac87fb1743c62d365795
Elaine <sosohn@gmail.com>
2022-06-01 14:19:09 -0700

    Add Link @Welcome

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
+      <Link to="new-user">New User</Link>
       <Routes>
         <Route path="new-user" element={<p>Welcome, new user!</p>} />
       </Routes>
