5dcc7f62d222b3489993d08885f2f78aa2d78cfd
Elaine <sosohn@gmail.com>
2022-06-01 14:04:16 -0700

    Change Redirect to Navigate @App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Redirect } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
@@ -11,7 +11,7 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
-          <Route path="/" element={<Redirect to="/welcome" />} />
+          <Route path="/" element={<Navigate to="/welcome" />} />
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
